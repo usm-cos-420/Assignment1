18b2cdc07558256d5e2f80e2ec4d3269cbd06dca
Bruce MacLeod <macleod@maine.edu>
2022-12-23 11:22:49 -0500

    Put a few recommendations in the top level routine

--- src/main/java/edu/usm/cos420/example1/App.java
+++ src/main/java/edu/usm/cos420/example1/App.java
@@ -7,7 +7,16 @@ import edu.usm.cos420.example1.view.impl.CItemView;
 
 /**
  * Top level application class that coordinates the calls to view and Controller
- *
+ *   
+ *   This serves as a starting point for your Assignment 1.
+ *       I have demonstrated the flow of control for a class called CItem (which is an example class)
+ *       You should examine all of the code and follow the flow of control before 
+ *       making classes for Customers, Inventory items, ...
+ *       
+ *   Work iteratively ! Do not try to solve the assignment in one go ! So, for example, 
+ *       you may want to start by defining the Customer class, then try to store and 
+ *       retrieve customers (you will need to make a Data Access class for Customers). 
+ *       Step by step wins the day !
  */
 public class App
 {
